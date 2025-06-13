0f041f53f330d2b2b2bcfe48cb8ccf8d387ca433
ElmonINC <igwesmsn@gmail.com>
2025-06-13 13:50:29 +0100

    setting the index layout

---
+++ data_elda/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    # Add your URL patterns here
+    # Example: path('', views.home, name='home'),
+    path('', views.index, name='index'),
+]
--- elda/urls.py
+++ elda/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('data_elda/', include('data_elda.urls')),
+    path('', include('data_elda.urls')),  # Redirect root URL to data_elda app
 ]
