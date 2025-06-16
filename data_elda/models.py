93668de1efc3aaffe4e07f38815e173dd702ce0f
ElmonINC <igwesmsn@gmail.com>
2025-06-16 08:08:27 +0100

    Form created

--- data_elda/models.py
+++ data_elda/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class UploadedFile(models.Model):
+    """
+    Model to store uploaded files.
+    """
+    file = models.FileField(upload_to='uploads/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(blank=True)
+
 # Create your models here.
--- data_elda/views.py
+++ data_elda/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+# Create your views here.
+from django.shortcuts import render, redirect
+from .forms import UploadFileForm
+from .models import UploadedFile
+from .utils import extract_text
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+
+
 
 def index(request):
     return render(request, 'data_elda/index.html')
 
-# Create your views here.
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            uploaded_file = form.save()
+            file_path = uploaded_file.file.path
+            text = extract_text(file_path)
+            uploaded_file.content = text
+            uploaded_file.save()
+            return redirect('index')  # Redirect back to the main page
+    else:
+        form = UploadFileForm()
+    return render(request, 'index.html', {'form': form})
+
+def search(request):
+    query = request.GET.get('q')
+    if query:
+        search_query = SearchQuery(query, search_type='websearch')
+        results = UploadedFile.objects.annotate(
+            rank=SearchRank(SearchVector('content', 'file'), search_query)
+        ).filter(rank__gte=0.1).order_by('-rank')
+    else:
+        results = UploadedFile.objects.none()
+    return render(request, 'search_results.html', {'results': results, 'query': query})
