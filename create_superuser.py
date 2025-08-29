3fd980e76f1255d92a6860f6fe303eefb3a61b03
ElmonINC <igwesmsn@gmail.com>
2025-08-29 15:31:27 +0100

    Sa111111

--- create_superuser.py
+++ create_superuser.py
@@ -1,22 +1,25 @@
 # create_superuser.py
 import os
 import django
-from django.contrib.auth import get_user_model
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elda.settings")
 django.setup()
 
+from django.contrib.auth import get_user_model
+
 User = get_user_model()
 
 username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "elmon")
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "igwesmsn@example.com")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "igwesmsn@gmail.com")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "Dip1tip2Dwaka@#elW")
 
 if not User.objects.filter(username=username).exists():
-    print(f"Creating superuser {username}...")
     User.objects.create_superuser(username=username, email=email, password=password)
+    print("Superuser created ✅")
 else:
-    print(f"Superuser {username} already exists. Resetting password...")
     u = User.objects.get(username=username)
-    u.set_password(password)
+    u.is_staff = True
+    u.is_superuser = True
+    u.set_password(password)  # reset in case old password was wrong
     u.save()
+    print("Superuser already exists, updated ✅")
